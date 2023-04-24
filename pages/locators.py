9ec738368e12a265dc212fae4e9cc71aaf2c3e73
Nikolai <niklas1076@yandex.ru>
2023-04-24 10:34:18 +0300

    Add login_page.py and add test to test_main_page.py

---
+++ pages/locators.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_FORGET_PASSWORD_LINK = (By.CSS_SELECTOR, "#login_form p > a")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary[name='login_submit']")
+
+
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary[name='registration_submit']")
--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,11 @@
+import pytest
 from selenium.webdriver.common.by import By
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
+
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -9,8 +13,16 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 
 
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_login_and_register_forms_should_be(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
